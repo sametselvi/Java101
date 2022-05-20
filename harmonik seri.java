00d5ba3284c1e3b78a61ceb177fd615f9842d021
sametselvi <103968854+sametselvi@users.noreply.github.com>
2022-05-20 16:29:32 +0300

    Add files via upload

---
+++ harmonik seri.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("N sayısı giriniz : ");
+        int n = input.nextInt();
+        double  result =0.0;
+        for ( int i = 1; i <=n ; i ++){
+            result += (1.0/i);
+
+        }
+        System.out.print(result);
+
+    }
+}
